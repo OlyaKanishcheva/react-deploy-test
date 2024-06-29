5a3386b9fc4ef2a151d8bdc9f62a8546be56fbe8
Olga Kanishcheva <kanishcheva@phystech.edu>
2024-06-29 21:03:25 +0400

    edit & up version

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,14 @@ export const App = () => {
 
     const increment = () => setCount(prev => prev + 1);
     const decrement = () => setCount(prev => prev - 1);
+    const reset = () => setCount(0);
 
     return (
         <div>
             <h1>value = {count}</h1>
             <button onClick={increment}>increment</button>
             <button onClick={decrement}>decrement</button>
+            <button onClick={reset}>reset</button>
 
             <div style={{display: 'flex', gap: 15}}>
                 <Link to={'about'}>about</Link>
